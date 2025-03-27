f67f1f72de46bbe2ed1f36afa0923b78c1b5722f
Yamanqui <yamanqui@chacala.school>
2025-03-27 16:02:39 -0600

    Added first scene and script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.transform.position = new Vector2(8,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
