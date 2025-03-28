c882293f1da6da521193b9d7f3e57cc25e63b783
Yamanqui <yamanqui@chacala.school>
2025-03-28 15:07:28 -0700

    Added basic physics.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float jumpPower;
+    private InputAction jumpAction;
+    private bool jumpTriggered;
+    new private Rigidbody2D rigidbody;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        this.transform.position = new Vector2(8,0);
+    private void Start() {
+        jumpAction = InputSystem.actions.FindAction("Jump");
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if(jumpAction.WasPressedThisFrame()){
+            jumpTriggered = true;
+        }
+    }
+    
+    private void FixedUpdate() {
+        if (jumpTriggered) {
+            rigidbody.linearVelocityY = jumpPower;
+            jumpTriggered = false;
+        }
     }
 }
